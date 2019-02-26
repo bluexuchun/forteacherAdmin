6b498d271faef2cedc6da1175a487b4230b60cb0
bluexuchun <345065695@qq.com>
2019-02-26 17:56:44 +0800

    增加时间的修改

--- src/pages/Arrange/index.js
+++ src/pages/Arrange/index.js
@@ -1,12 +1,13 @@
 import React, { Component, Suspense } from 'react';
 import router from 'umi/router';
 import { connect } from 'dva';
-import { Input, Button, Row, Col, Calendar } from 'antd';
+import { Input, Button, Row, Col, Calendar, TimePicker } from 'antd';
 import PageLoading from '@/components/PageLoading';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import GridContent from '@/components/PageHeaderWrapper/GridContent';
 import IntroCommon from '@/components/IntroCommon';
 import styles from './index.less';
+import moment from 'moment';
 
 @connect()
 class ArrangeIndex extends Component {
@@ -16,29 +17,71 @@ class ArrangeIndex extends Component {
   }
 
   componentWillMount = () => {
-    console.log(this.props)
+    console.log(this.props);
   };
 
   onPanelChange = (value, mode) => {
     console.log(value, mode);
-  }
+  };
 
   render() {
     const { match, children, location } = this.props;
+
+    const minutes = 'm';
+    const second = 'ss';
 
     return (
       <GridContent>
         <Suspense fallback={<PageLoading />}>
           <div className={styles.tit}>开放时间</div>
-          <Row gutter={12} style={{ padding: '20px 0px' }}>
-            <Col span={8} style={{ textAlign: 'center' }}>
-                <Calendar fullscreen={false} onPanelChange={() => this.onPanelChange} />
+          <Row gutter={12} style={{ padding: '20px 0px', position: 'relative' }}>
+            <div className={styles.line} />
+
+            <Col span={10} className={styles.mainCol}>
+              <div
+                style={{
+                  width: '75%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Calendar
+                  style={{ width: '100%' }}
+                  fullscreen={false}
+                  onPanelChange={() => this.onPanelChange}
+                />
+              </div>
             </Col>
-            <Col span={1}>
-              <div className={styles.line}></div>
-            </Col>
-            <Col span={12}>
-              
+
+            <Col span={14}>
+              <div className={styles.timemain}>
+                <Row className={styles.timegroup}>
+                  <Col span={10} className={styles.timeitem}>
+                    <TimePicker defaultValue={moment('9', minutes)} format={minutes} />
+                    <TimePicker defaultValue={moment('12', second)} format={second} />
+                  </Col>
+                  <Col span={2} className={styles.timecenter}>
+                    <div className={styles.itemline} />
+                  </Col>
+                  <Col span={10} className={styles.timeitem}>
+                    <TimePicker defaultValue={moment('9', minutes)} format={minutes} />
+                    <TimePicker defaultValue={moment('12', second)} format={second} />
+                  </Col>
+                </Row>
+                {/* <div className={styles.timegroup}>
+                  <div className={styles.timeitem}>
+                    <TimePicker defaultValue={moment('9', minutes)} format={minutes} />
+                    <TimePicker defaultValue={moment('12', second)} format={second} />
+                  </div>
+
+                  <div className={styles.timeitem}>
+                    <TimePicker defaultValue={moment('9', minutes)} format={minutes} />
+                    <TimePicker defaultValue={moment('12', second)} format={second} />
+                  </div>
+                </div> */}
+              </div>
             </Col>
           </Row>
         </Suspense>
